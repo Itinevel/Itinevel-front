43dfcca263f5be046b6c44cf6a860d113e6a67f7
Salim <salimaloui406@gmail.com>
2024-10-12 14:33:47 +0100

    fix

--- app/preview-plan/[planId]/page.tsx
+++ app/preview-plan/[planId]/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { useParams } from 'next/navigation';
-import PreviewPlan from '@/app/preview-plan/page'; // Adjust the path based on your structure
+import PreviewPlan from '@/components/view/PreviewPlanComponent';// Adjust the path based on your structure
 
 const PreviewPlanPage = () => {
-  const params = useParams(); // Use useParams to get dynamic route params
-  const { planId } = params;
+  const params = useParams();
+  const planId = typeof params?.planId === 'string' ? params.planId : undefined;
 
   if (!planId) {
     return <div>Loading...</div>;
   }
 
-  return <PreviewPlan planId={planId as string} />;
+  return <PreviewPlan planId={planId} />;
 };
 
 export default PreviewPlanPage;
