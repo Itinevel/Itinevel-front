0d9c95f52c0408e0f252e670b1fc03a919b2092e
Salim <salimaloui406@gmail.com>
2024-10-14 15:35:24 +0100

    fix

--- components/utils/navbar.tsx
+++ components/utils/navbar.tsx
@@ -88,7 +88,7 @@ const Navbar: React.FC<NavbarProps> = ({ className, onNavigate }) => {
   const confirmBecomeSeller = async () => {
     try {
       // Send request to update roles in the database
-      const response = await fetch(`http://localhost:3001/api/users/${userId}/updateRole`, {
+      const response = await fetch(`http://itinevel-back.vercel.app/api/users/${userId}/updateRole`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
